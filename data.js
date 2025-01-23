2d7403272728efabcbc951a41c916ff42fa53f41
Anuna29 <anusukh29@gmail.com>
2025-01-22 22:59:25 -0600

    added data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+let items = [
+  { id: 1, name: "Item 1", description:"Description for Item 1", category: "Category A" },
+  { id: 2, name: "Item 2", description:"Description for Item 2", category: "Category B" },
+  { id: 3, name: "Item 3", description:"Description for Item 2", category: "Category A" },
+  { id: 4, name: "Item 4", description:"Description for Item 4", category: "Category B" },
+  { id: 5, name: "Item 5", description:"Description for Item 5", category: "Category A" },
+];
+
+ module.exports = { items };
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const express = require("express");
-
-const app = express();
-
-const PORT = 8000;
-
 //Operation:
 // C --> create (POST method)
 // R --> read (GET method)
@@ -16,23 +10,49 @@ const PORT = 8000;
 // npm install nodemon --save-dev
 
 //TO RUN THE SERVER:
-// nodemon index.js [index.js is the name of the file¬¬¬¬¬]
+// nodemon index.js [index.js is the name of the file]
+
+const express = require("express");
+const { items } = require("./data");
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
 
 app.get("/", (request,response) => {
   response.send("GET request received!");
 })
 
-app.post("/", (request,response) => {
-  response.send("POST request received!");
+app.get("/items", (request,response) => {
+  response.status(200).json(items);
+})
+
+app.get("/items:id", (request,response) => {
+  const { id } = request.params;
+
+  const item = items.find((item) => item.id === Number(id));
+
+  if (!item){
+    response.status(404).json({
+      error: "Item not found!"
+    });
+  }
+  response.status(200).json(item);
 });
 
-app.put("/", (request,response) => {
-  response.send("PUT request received!");
-});
 
-app.delete("/", (request,response) => {
-  response.send("DELETE request received!");
-});
+// app.post("/", (request,response) => {
+//   response.send("POST request received!");
+// });
+
+// app.put("/", (request,response) => {
+//   response.send("PUT request received!");
+// });
+
+// app.delete("/", (request,response) => {
+//   response.send("DELETE request received!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
