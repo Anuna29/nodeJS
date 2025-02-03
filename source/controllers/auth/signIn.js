f15df1e758f14d65ffde1542f8c52609496ff605
Anuna29 <anusukh29@gmail.com>
2025-02-03 15:19:08 -0600

    authentication

--- source/controllers/auth/signIn.js
+++ source/controllers/auth/signIn.js
@@ -1,5 +1,30 @@
+const User = require("../../models/auth");
+const bcrypt = require("bcrypt");
+
 const signIn = async (req, res) => {
-  res.send("Sign In")
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await User.findOne({ email });
+
+  if(!user){
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if(!isMatch){
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  return res.status(200).json({
+    _id: user._id,
+    email: user.email,
+    username: user.username,
+  });
 };
 
 module.exports = {
--- source/controllers/auth/signUp.js
+++ source/controllers/auth/signUp.js
@@ -1,5 +1,45 @@
+const validator = require("validator");
+const User = require("../../models/auth");
+const bcrypt = require("bcrypt");
+
 const signUp = async (req, res) => {
-  res.send("Sign Up")
+  const { email, password, username } = req.body;
+
+  if (!email || !password || !username) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  if(!validator.isEmail(email)){
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  if(!validator.isStrongPassword(password)){
+    return res.status(400).json({ message: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character" });
+  }
+
+  try{
+    const user = await User.findOne({email});
+
+    if(user){
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    
+    //Salting is adding random characters to the password
+    const salt = await bcrypt.genSalt(10);
+
+    // Hashing is converting the password into a long string of characters
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+      username,
+    });
+
+    return res.status(201).json(newUser);
+  }catch (error){
+    res.status(500).json({ message: error.message});``
+  }
 };
 
 module.exports = {
